6f94519f3a9bf44bf14ef61ec7f991521dcf4c4d
smr <2152918099@qq.com>
2024-04-16 16:40:27 +0800

    update

---
+++ program1/1538TypeWriter.c
@@ -0,0 +1,53 @@
+#include "stdio.h"
+#include "string.h"
+
+int main(){
+    char str[505];
+    while (scanf("%s",str)!=EOF){
+        char outputStr[505];
+        int len_output=0,cursor=0;
+        for (int i = 0; i < strlen(str); ++i) {
+            //插入字母
+            if((str[i]>='a'&&str[i]<='z')||(str[i]>='A'&&str[i]<='Z')){
+                for (int j = len_output; j >=cursor+1 ; --j) {
+                    outputStr[j]=outputStr[j-1];
+                }
+                outputStr[cursor]=str[i];
+                cursor++;
+                len_output++;
+            }
+            //首尾跳转
+            if(str[i]=='['){
+                cursor=0;
+            }
+            if(str[i]==']'){
+                cursor=len_output;
+            }
+            //光标移动
+            if(str[i]=='<'&&cursor!=0){
+                cursor--;
+            }
+            if(str[i]=='>'&&cursor!=len_output){
+                cursor++;
+            }
+            //删除
+            if(str[i]=='-'&&cursor!=0){
+                for (int j = cursor-1; j < len_output-1; ++j) {
+                    outputStr[j]=outputStr[j+1];
+                }
+                len_output--;
+            }
+            if(str[i]=='+'&&cursor!=len_output){
+                for (int j = cursor; j < len_output - 1; ++j) {
+                    outputStr[j]=outputStr[j+1];
+                }
+                len_output--;
+            }
+        }
+        for (int i = 0; i < len_output; ++i) {
+            printf("%c",outputStr[i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
