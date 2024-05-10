2d6fce2a56f6c02871b096f5c81849e4d2c99137
smr <2152918099@qq.com>
2024-05-10 19:11:35 +0800

    1586积

---
+++ program2/1586Product.cpp
@@ -0,0 +1,52 @@
+#include "iostream"
+#include "algorithm"
+
+using namespace std;
+
+const int mod = 1000000007;
+
+long long int maxProduct(long long int *nums, int n, int k) {
+    sort(nums, nums + n);
+
+    int left = 0, right = n - 1;
+    long long int product = 1;
+    long long int flag = 1;
+
+    if (k % 2) {
+        product = nums[right];
+        right--;
+        k--;
+        if (product < 0) {
+            flag = -1;
+        }
+    }
+
+    while (k) {
+        long long int lp = nums[left] * nums[left + 1];
+        long long int rp = nums[right] * nums[right - 1];
+
+        if (lp * flag > rp * flag) {
+            product = lp % mod * product % mod;
+            left += 2;
+        } else {
+            product = rp % mod * product % mod;
+            right -= 2;
+        }
+        k -= 2;
+    }
+    return product;}
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    while (T--) {
+        int n, k;
+        long long int nums[10005];
+        scanf("%d %d", &n, &k);
+        for (int i = 0; i < n; ++i) {
+            scanf("%I64d", &nums[i]);
+        }
+        printf("%I64d\n", maxProduct(nums, n, k));
+    }
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,38 +1,53 @@
-#include "iostream"
-#include "queue"
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int minOperation(int x, int y) {
-    queue<int> que;
-    int dist[100005] = {0};
-    que.push(x);
-    dist[x] = 0;
+typedef long long LL;
 
-    while (!que.empty()) {
-        int num = que.front();
-        que.pop();
+const int N = 100010, mod = 1000000009;
 
-        if (num == y)return dist[num];
+int a[N];
 
-        int nextNumbers[4] = {num * 2, num + 1, num / 2, num - 1};
-        for (int next: nextNumbers) {
-            if (next >= 1 && next <= 100000 && dist[next] == 0) {
-                que.push(next);
-                dist[next] = dist[num] + 1;
-            }
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    for (int i = 0; i < n; i ++) scanf("%d", &a[i]);
+
+    sort(a, a + n);
+
+    int l  = 0, r = n - 1;                                     // 双指针初始化
+    int ans = 1;                                               // 乘积初始化
+    int sign  = 1;                                             // 符号初始化
+
+    if(k % 2)                                                  // k 为奇数
+    {
+        ans = a[r];                                            // 取出最大的数
+        r --;                                                  // r指针 左移
+        k --;                                                  // k 变为偶数
+
+        if(ans < 0) sign = -1;                                 // 最大值为负数，则所有数都是负数，结果也为负，所以要让绝对值小
+    }
+
+    while(k)
+    {
+        LL x = (LL)a[l] * a[l + 1], y =(LL)a[r] * a[r - 1];    // 左右两边同时取一对乘积，比较大小
+        if(x * sign > y * sign)
+        {
+            ans = x % mod * ans % mod;
+            l += 2;                                            // l指针 右移
         }
+        else
+        {
+            ans = y % mod * ans % mod;
+            r -= 2;                                            // r指针 左移
+        }
+        k -= 2;                                                // 个数减二
     }
-    return -1;
-}
 
-int main() {
-    int T;
-    scanf("%d", &T);
-    while (T--) {
-        int x, y;
-        scanf("%d %d", &x, &y);
-        printf("%d\n", minOperation(x, y));
-    }
+    cout << ans << endl;
     return 0;
 }
