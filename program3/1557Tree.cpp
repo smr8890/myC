35b706d859ffd46de6444c582a489cb7a0d8e868
smr <2152918099@qq.com>
2024-05-23 20:54:39 +0800

    1557树

--- program3/1557Tree.cpp
+++ program3/1557Tree.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
 struct node {
@@ -13,37 +12,38 @@ bool cmp(node a, node b) {
     return a.w > b.w;
 }
 
-int find_parent(int *parent, int x) {
+int find_parent(int x, int *parent) {
     if (parent[x] == x) {
         return parent[x];
     }
-    return parent[x] = find_parent(parent, parent[x]); //路径压缩
+    return parent[x] = find_parent(parent[x], parent);
 }
 
-int result(node *a, int n, int m) {
-    int parent[1005];
-    for (int i = 0; i <= 1004; i++) {
+int result(int n, int m, node *a) {
+    int parent[2005];
+    for (int i = 0; i <= n; i++) {
         parent[i] = i;
     }
+
     sort(a, a + m, cmp);
     int sum = 0, count = 0;
     for (int i = 0; i < m; i++) {
-        int t1 = find_parent(parent, a[i].u);
-        int t2 = find_parent(parent, a[i].v);
+        int t1 = find_parent(a[i].u, parent);
+        int t2 = find_parent(a[i].v, parent);
         if (t1 != t2) {
             sum += a[i].w;
-            parent[t2] = t1;
             count++;
+            parent[t2] = t1;
             if (count == n - 1) {
                 break;
             }
         }
     }
-    int sum1=0;
-    for (int i=0;i<m;i++) {
-        sum1+=a[i].w;
+    int sum0 = 0;
+    for (int i = 0; i < m; i++) {
+        sum0 += a[i].w;
     }
-    return sum1-sum;
+    return sum0 - sum;
 }
 
 int main() {
@@ -52,12 +52,11 @@ int main() {
     while (T--) {
         int n, m;
         scanf("%d %d", &n, &m);
-        node a[1005];
+        node a[20005];
         for (int i = 0; i < m; i++) {
-            scanf("%d %d %d", &a[i].u, &a[i].v, a[i].w);
+            scanf("%d %d %d", &a[i].u, &a[i].v, &a[i].w);
         }
-        int x=result(a,n,m);
-        printf("%d\n",x);
+        printf("%d\n", result(n, m, a));
     }
     return 0;
 }
